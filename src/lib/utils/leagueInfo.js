ea1e7196db0662fd06d1aa2dbeec3837fd5b69a4
MajorMilou <114604499+MajorMilou@users.noreply.github.com>
2023-09-07 23:37:19 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = 989903632843751424; // your league ID
+export const leagueID = "989903632843751424"; // your league ID
 export const leagueName = "League of (French) Legends"; // your league name
 export const dues = 0; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
